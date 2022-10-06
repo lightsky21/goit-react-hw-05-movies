2917e026d9373f4cddc6f8501739c6c6bfa6b291
lightsky21 <90994603+lightsky21@users.noreply.github.com>
2022-10-06 00:56:18 +0000

    Deploying to gh-pages from @ lightsky21/goit-react-hw-05-movies@80002b4dc7b4a439b529fe0cf68e1d711b65a3cf 🚀

---
+++ static/js/75.4ae1fe42.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{75:function(e,t,i){i.r(t);var a=i(184);t.default=function(){return(0,a.jsxs)("main",{children:[(0,a.jsx)("h1",{children:"Welcome"}),(0,a.jsx)("img",{src:"https://via.placeholder.com/960x240",alt:""}),(0,a.jsx)("p",{children:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, laboriosam placeat incidunt rem illum animi nemo quibusdam quia voluptatum voluptate."})]})}}}]);
+//# sourceMappingURL=75.4ae1fe42.chunk.js.map
